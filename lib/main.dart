e3c67cb7d23a042f1d73f675979ea5473232e2eb
bstawy <125200508@sha.edu.eg>
2023-09-26 01:19:13 +0300

    configure home layout route name

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:islami/layout/home_layout.dart';
 
 import 'core/theme/application_theme.dart';
 void main() {
@@ -7,27 +8,30 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static String currentLanguage = 'ar' , currentMode = 'light';
+
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Islami',
 
       // Specify delegates and supported languages
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
+      locale: Locale(currentLanguage),
 
       themeMode: ThemeMode.light,
       theme: ApplicationTheme.lightTheme,
       darkTheme: ApplicationTheme.darkTheme,
 
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Islami'),
-        ),
-      ),
+      initialRoute: HomeLayout.routeName,
+      routes: {
+        HomeLayout.routeName : (context) => const HomeLayout(),
+      },
     );
   }
 }
