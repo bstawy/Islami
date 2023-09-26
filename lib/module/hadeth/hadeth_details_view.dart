01374ced2c32d2ee65902a4e98219a1cd13f9932
bstawy <125200508@sha.edu.eg>
2023-09-26 16:37:08 +0300

    finish hadeth screen

---
+++ lib/module/hadeth/hadeth_details_view.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:islami/module/hadeth/hadeth_view.dart';
+import '../../main.dart';
+
+class HadethDetailsView extends StatelessWidget {
+  static const String routeName = 'hadeth-details-view';
+  List<String> hadethContent = const [];
+
+  HadethDetailsView({super.key});
+
+  getHadethContent(String content) {
+    hadethContent = content.split('\n');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var args = ModalRoute.of(context)?.settings.arguments as HadethData;
+    var mediaQuery = MediaQuery.of(context).size;
+    var theme = Theme.of(context);
+    var locale = AppLocalizations.of(context)!;
+
+    getHadethContent(args.content);
+
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            (MyApp.currentMode == 'light')
+                ? 'assets/images/background.png'
+                : 'assets/images/background_dark.png',
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(locale.islami),
+        ),
+        body: Container(
+          margin:
+          const EdgeInsets.only(top: 30, right: 30, left: 30, bottom: 100),
+          padding: const EdgeInsets.all(40),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: theme.colorScheme.secondary.withOpacity(0.8),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                args.title,
+                style: theme.textTheme.titleMedium,
+                textAlign: TextAlign.center,
+              ),
+              Divider(
+                color: theme.colorScheme.primary,
+                indent: 25,
+                endIndent: 25,
+                height: 0,
+                thickness: 2,
+              ),
+              SizedBox(height: 10),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: hadethContent.length,
+                  itemBuilder: (context, index) => Text(
+                    hadethContent[index],
+                    style: theme.textTheme.bodyMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/module/hadeth/hadeth_view.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:islami/module/hadeth/hadeth_details_view.dart';
+
+class HadethView extends StatefulWidget {
+  const HadethView({super.key});
+
+  @override
+  State<HadethView> createState() => _HadethViewState();
+}
+
+class _HadethViewState extends State<HadethView> {
+  final List<HadethData> allHadethContent = [];
+
+  @override
+  Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context).size;
+    var locale = AppLocalizations.of(context)!;
+    var theme = Theme.of(context);
+
+    if (allHadethContent.isEmpty) {
+      readFile();
+    }
+
+    return Column(
+      children: [
+        Image.asset(
+          'assets/images/ahadeth_header.png',
+          width: mediaQuery.width / 2,
+          height: mediaQuery.height / 3.8,
+          fit: BoxFit.cover,
+        ),
+        SizedBox(height: mediaQuery.height * 0.01),
+        Divider(
+          color: theme.colorScheme.primary,
+          indent: 10,
+          endIndent: 10,
+          height: 0,
+          thickness: 2,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5.5),
+          child: Text(
+            locale.hadeth,
+            style: theme.textTheme.titleLarge,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Divider(
+          color: theme.colorScheme.primary,
+          indent: 10,
+          endIndent: 10,
+          height: 0,
+          thickness: 2,
+        ),
+        Expanded(
+          child: ListView.separated(
+            itemBuilder: (context, index) => GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, HadethDetailsView.routeName,
+                    arguments: allHadethContent[index]);
+              },
+              child: Text(
+                allHadethContent[index].title,
+                style: theme.textTheme.titleMedium,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            separatorBuilder: (context, index) => Divider(
+              color: theme.colorScheme.primary,
+              indent: 75,
+              endIndent: 75,
+              height: 10,
+              thickness: 1.2,
+            ),
+            itemCount: allHadethContent.length,
+          ),
+        ),
+      ],
+    );
+  }
+
+  void readFile() async {
+    String text = await rootBundle.loadString('assets/files/ahadeth.txt');
+    List<String> allHadeth = text.split('#');
+
+    for (int i = 0; i < allHadeth.length; i++) {
+      String singleHadeth = allHadeth[i].trim();
+      String title = singleHadeth.substring(0, singleHadeth.indexOf('\n'));
+      String content = singleHadeth.substring(singleHadeth.indexOf('\n') + 1);
+
+      setState(() {
+        allHadethContent.add(HadethData(title: title, content: content));
+      });
+    }
+  }
+}
+
+class HadethData {
+  final String title, content;
+  const HadethData({required this.title, required this.content});
+}
