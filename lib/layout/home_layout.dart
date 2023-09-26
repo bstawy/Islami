30b9bc854db6411f777cc3ad4cd4fe4dfdc2acab
bstawy <125200508@sha.edu.eg>
2023-09-26 03:23:12 +0300

    add application screens and navigate between them using bottom navigation bar

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:islami/main.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:islami/module/quran/quran_view.dart';
 
-class HomeLayout extends StatelessWidget {
+import '../module/hadeth/hadeth_view.dart';
+import '../module/radio/radio_view.dart';
+import '../module/settings/settings_view.dart';
+import '../module/tasbeh/tasbeh_view.dart';
+
+class HomeLayout extends StatefulWidget {
   static const String routeName = 'home-layout';
 
   const HomeLayout({super.key});
+
+  @override
+  State<HomeLayout> createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
+  int selectedIndex = 0;
+
+  List<Widget> screens = const [
+    QuranView(),
+    HadethView(),
+    TasbehView(),
+    RadioView(),
+    SettingsView()
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +47,45 @@ class HomeLayout extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: Text(locale.islami),
+        ),
+        body: screens[selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+          currentIndex: selectedIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage('assets/images/quran_bottom_nav_icon.png'),
+              ),
+              label: locale.quran,
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage('assets/images/ahadeth_bottom_nav_icon.png'),
+              ),
+              label: locale.hadeth,
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage('assets/images/sebha_bottom_nav_icon.png'),
+              ),
+              label: locale.tasbeh,
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage('assets/images/radio_bottom_nav_icon.png'),
+              ),
+              label: locale.radio,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: locale.settings,
+            ),
+          ],
         ),
       ),
     );
