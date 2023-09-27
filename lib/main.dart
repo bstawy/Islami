97df4a52fd318bf404770c025b7449be5ad72cbe
bstawy <125200508@sha.edu.eg>
2023-09-27 16:20:17 +0300

    support provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:islami/core/provider/app_provider.dart';
+import 'package:provider/provider.dart';
 import 'core/theme/application_theme.dart';
 import 'package:islami/layout/home_layout.dart';
 import 'package:islami/module/quran/quran_details_view.dart';
 import 'module/hadeth/hadeth_details_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AppProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  static String currentLanguage = 'en' , currentMode = 'light';
-
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    var appProvider = Provider.of<AppProvider>(context);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Islami',
@@ -24,17 +32,17 @@ class MyApp extends StatelessWidget {
       // Specify delegates and supported languages
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      locale: Locale(currentLanguage),
+      locale: Locale(appProvider.currentLocal),
 
-      themeMode: ThemeMode.light,
+      themeMode: appProvider.currentTheme,
       theme: ApplicationTheme.lightTheme,
       darkTheme: ApplicationTheme.darkTheme,
 
       initialRoute: HomeLayout.routeName,
       routes: {
-        HomeLayout.routeName : (context) => const HomeLayout(),
-        QuranDetailsView.routeName : (context) => const QuranDetailsView(),
-        HadethDetailsView.routeName : (context) => HadethDetailsView(),
+        HomeLayout.routeName: (context) => const HomeLayout(),
+        QuranDetailsView.routeName: (context) => const QuranDetailsView(),
+        HadethDetailsView.routeName: (context) => HadethDetailsView(),
       },
     );
   }
